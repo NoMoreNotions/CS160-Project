4be0db56fb046595de01b6a46d8cdb2036564a7b
NoMoreNotions <david.truong@sjsu.edu>
2021-11-04 20:45:59 -0700

    Basic Python files for website

---
+++ HTML/website/auth.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login')
+def login():
+    return "<h1>LI<h1>"
+
+
+@auth.route('/logout')
+def logout():
+    return "<h1>LO<h1>"
+
+
+@auth.route('/sign-up')
+def sign_up():
+    return "<h1>SU<h1>"
