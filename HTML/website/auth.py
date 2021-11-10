9a93badf7f15e803013490fab0feb2fdd90d47f0
NoMoreNotions <david.truong@sjsu.edu>
2021-11-09 19:33:33 -0800

    Updated base python files

--- HTML/website/auth.py
+++ HTML/website/auth.py
@@ -1,18 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login')
 def login():
-    return "<h1>LI<h1>"
+    return render_template("login.html", text="Testing")
 
 
 @auth.route('/logout')
 def logout():
-    return "<h1>LO<h1>"
+    return "<p>Logged out</p>"
 
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<h1>SU<h1>"
+    return render_template("sign_up.html")
