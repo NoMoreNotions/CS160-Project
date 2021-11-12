b5f632dc40e5faf21ef0248c512a948a1d9a0037
NoMoreNotions <david.truong@sjsu.edu>
2021-11-12 12:58:11 -0800

    Wrong branch, my bad

--- HTML/website/auth.py
+++ HTML/website/auth.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html", text="Testing")
 
@@ -13,6 +13,11 @@ def logout():
     return "<p>Logged out</p>"
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
     return render_template("sign_up.html")
