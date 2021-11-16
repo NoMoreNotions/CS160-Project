349965ea07402a5eaee639db4a5c6d5578aaa7cc
NoMoreNotions <david.truong@sjsu.edu>
2021-11-15 22:17:59 -0800

    Update files

--- HTML/website/models.py
+++ HTML/website/models.py
@@ -9,3 +9,33 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+
+
+class AppUsers(db.Model):
+    userId = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(150))
+    age = db.Column(db.Integer)
+    weight = db.Column(db.Integer)
+    hieght = db.Column(db.Integer)
+    goalWeight = db.Column(db.Integer)
+
+
+class CalorieInfo(db.Model):
+    itemId = db.Column(db.Integer, primary_key=True)
+    foodName = db.Column(db.String(200), nullable=False)
+    calorie = db.Column(db.Integer, nullable=False)
+
+
+class FoodHistory(db.Model):
+    itemId = db.Column(db.Integer)
+    userId = db.Column(db.Integer)
+    quantity = db.Column(db.Integer, nullable=False)
+    dateInfo = db.Column(db.date)
